b095865ef67ee6fc43ee7c477ef5a71e25961dd3
Thiem Huynh <thiem.huynh0@gmail.com>
2024-03-19 00:37:48 -0400

    pets page

---
+++ src/app/pets/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Page(){
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>All Pets</h1>
+      </div>
+    </section>
+  )
+}
