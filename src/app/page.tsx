407fad0738e27f3983499cb3fb35a8f0fe787b2a
Thiem Huynh <thiem.huynh0@gmail.com>
2024-03-22 18:09:14 -0400

    adding sample pdf buttons and button for downloads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the new AGA Interface / WebTool</h1>
       <a href="./dogList">Go search Dog List</a>
+      <h2>Sample Link Below</h2>
+      <Button variant="outline" className="bg-slate-300">
+        <a href="/agaFiles/sample.pdf" target="blank" rel="noopener noreferrer">
+          Download sample.pdf File
+        </a>
+      </Button>
     </div>
   );
 }
